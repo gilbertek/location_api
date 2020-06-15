a583444be85d59d0e18275dc7629f215ac70c2fc
Gilbert F. Sewovoe-Ekoue <gilberts55@hotmail.com>
2020-06-15 08:44:16 -0400

    Initial commit

---
+++ config/initializers/generators.rb
@@ -0,0 +1,8 @@
+Rails.application.config.generators do |g|
+  g.factory_bot false
+  g.helper false
+  g.javascripts false
+  g.stylesheets false
+  g.test_framework :rspec, view_specs: false, routing_specs: false
+  g.template_engine nil
+end
