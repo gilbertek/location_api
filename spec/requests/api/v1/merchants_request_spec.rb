cf6e2ece37de6052ea356878a435929812a960f9
Gilbert F. Sewovoe-Ekoue <gilberts55@hotmail.com>
2020-06-16 13:17:17 -0400

    requests specs

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Merchants", type: :request do
+  describe 'Api/V1/merchants' do
+    before do
+      5.times { FactoryBot.create(:merchant) }
+    end
 
+    it 'returns a list of all merchants' do
+      get api_v1_merchants_path
+
+      expect(response).to be_successful
+    end
+  end
 end
--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Users", type: :request do
+  describe 'Api/V1/users' do
+    before do
+      5.times { FactoryBot.create(:user) }
+    end
 
+    it 'returns a list of all users' do
+      get api_v1_users_path
+
+      expect(response).to be_successful
+      expect(JSON.parse(response.body).length).to equal(5)
+    end
+  end
 end
