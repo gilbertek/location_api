217083f873f90688add241024b6e3747d20b8d78
Gilbert F. Sewovoe-Ekoue <gilberts55@hotmail.com>
2020-06-16 12:50:33 -0400

    Models specs

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    it 'is not valid with missing name' do
+      expect(Merchant.new).to_not be_valid
+    end
+
+    it 'is valid when name is present' do
+      subject = FactoryBot.create(:merchant, merchant_name: 'Starbuck')
+      expect(subject).to be_valid
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    it 'is valid with valid attributes' do
+      subject = FactoryBot.create(:user)
+      expect(subject).to be_valid
+    end
+  end
 end
